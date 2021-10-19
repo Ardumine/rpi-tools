aa3ae16c92d6a5c45d95f31e14f5bcea13136836
Ardumine <61353807+Ardumine@users.noreply.github.com>
2021-10-19 14:19:35 +0100

    Update install.py

--- install.py
+++ install.py
@@ -12,9 +12,29 @@ f.write("sudo apt full-upgrade \n")
 f.write("sudo apt auto-remove \n")
 f.close()
 os.system("sudo chmod +x /usr/bin/updatepi")
-installrdp = input("Install Xrdp(Microsoft remote desktop)? y/n")
+
+installrdp = input("Install Xrdp(Microsoft remote desktop)? y/n:>")
+installcode = input("Install VS code(A IDE for python, etc...)? y/n:>")
+installffmeg = input("Install FFMPEG? y/n:>")
+installpygame = input("Install Pygmae\turle for python? y/n:>")
+
 if installrdp == "y":
 	os.system("sudo apt-get install xrdp")
 else:
 	print("Skiping rdp installer...")
+	
+if installcode == "y":
+	os.system("sudo apt-get install code")
+else:
+	print("Skiping vs code installer...")
+	
+if installffmeg == "y":
+	os.system("sudo apt-get install ffmpeg")
+else:
+	print("Skiping ffmpeg installer...")
+
+if installpygame == "y":
+	os.system("sudo pip3 install pygame")
+else:
+	print("Skiping ffmpeg installer...")
 print("Installlation complete!")
